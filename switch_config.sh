a4b1990829cbd04683ade931c60898970b6c69bf
Javier Zetina <javierzetina.z3@gmail.com>
2023-07-09 23:06:33 -0600

    HU002 - Ajuste de claves ssh agregadas al ssh-agent

--- switch_config.sh
+++ switch_config.sh
@@ -4,6 +4,8 @@
 # h-config: Cambia la configuración ssh para usario HVite
 # z-config: Cambia la configuración ssh para usuario JZetina
 
+# dt Establece la versión de node apta para trabajar con dt
+
 h_config=h-config
 z_config=z-config
 
@@ -15,14 +17,31 @@ else
   exit
 fi
 
+if [ "$1" != "$z_config" -a "$1" != "$h_config" ]; then
+  echo La configuración introducida es invalida
+  exit
+fi
+
+rm ~/.ssh/known_hosts
+eval "$(ssh-agent -s)"
+
+
 if [ "$1" = "$h_config" ]; then
   cp ~/workspace/h-config/config ~/.ssh
   cp ~/workspace/h-config/.gitconfig ~/
+
+  # TODO: Reemplzar por un bucle que recorra cada archivo ssh
+  ssh-add ~/workspace/h-config/bitbucket-HazaelDesarrollaTech
+  ssh-add ~/workspace/h-config/gitlab-HazaelQuo
 
   echo Se ha modificado la configuración para HVite
 elif [ "$1" = "$z_config" ]; then
   cp ~/workspace/z-config/config ~/.ssh
   cp ~/workspace/z-config/.gitconfig ~/
+
+  # TODO: Reemplzar por un bucle que recorra cada archivo ssh
+  ssh-add ~/workspace/z-config/gitlab_access
+  ssh-add ~/workspace/z-config/github_access
 
   echo Se ha modificado la configuración para JZetina
 else
