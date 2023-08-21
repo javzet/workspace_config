1c46f7c173f222f30c516aa5c1b96f3c9d4e58dc
Javier Zetina <javierzetina.z3@gmail.com>
2023-08-20 21:06:24 -0600

    Configuración actualizada

---
+++ .zprofile
@@ -0,0 +1,16 @@
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+alias switch_config=~/workspace/switch_config.sh
+
+alias node_default='nvm use default'
+alias node_dt='nvm use dt'
+alias workspace='cd ~/workspace'
+alias vcurrent_config='cat ~/.gitconfig'
+alias quo='cd ~/workspace/quo'
+alias quo-h='cd ~/workspace/quo-h'
+alias dt='cd ~/workspace/dt'
+alias swc-z='switch_config z-config'
+alias swc-h='switch_config h-config'
+alias npms='npm start'
+alias vscr='code . & clear & npms'
--- test/shell-script/switch_config.sh
+++ test/shell-script/switch_config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Introducir la configuración a usar:
-# h-config: Cambia la configuración ssh para usario HVite
+# h-config: Cambia la configuración ssh para usuario HVite
+# h-config-d: Cambia la configuración ssh para usuario HVite para DT
 # z-config: Cambia la configuración ssh para usuario JZetina
 
 h_config=h-config
+h_config_d=h-config-d
 z_config=z-config
 
 
@@ -18,6 +20,11 @@ fi
 if [ "$1" = "$h_config" ]; then
   cp ~/workspace/h-config/config ~/.ssh
   cp ~/workspace/h-config/.gitconfig ~/
+
+  echo Se ha modificado la configuración para HVite
+elif [ "$1" = "$h_config_d" ]; then
+  cp ~/workspace/h-config/config ~/.ssh
+  cp ~/workspace/h-config/especial-dt/.gitconfig ~/
 
   echo Se ha modificado la configuración para HVite
 elif [ "$1" = "$z_config" ]; then
