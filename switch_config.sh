0708bd98bf8d03fab8c7e13a1836d21ac3fa177a
Javier Zetina <javierzetina.z3@gmail.com>
2023-09-15 19:26:48 -0600

    Actualización de configuraciones

--- switch_config.sh
+++ switch_config.sh
@@ -8,7 +8,7 @@
 
 h_config=h-config
 z_config=z-config
-
+dt_config=dt-config
 
 if [ "$1" -a -n "$1" ]; then
   echo Usando la configuración $1
@@ -17,7 +17,7 @@ else
   exit
 fi
 
-if [ "$1" != "$z_config" -a "$1" != "$h_config" ]; then
+if [ "$1" != "$z_config" -a "$1" != "$h_config" -a "$1" != "$dt_config" ]; then
   echo La configuración introducida es invalida
   exit
 fi
@@ -31,7 +31,6 @@ if [ "$1" = "$h_config" ]; then
   cp ~/workspace/h-config/.gitconfig ~/
 
   # TODO: Reemplzar por un bucle que recorra cada archivo ssh
-  ssh-add ~/workspace/h-config/bitbucket-HazaelDesarrollaTech
   ssh-add ~/workspace/h-config/gitlab-HazaelQuo
   ssh-add ~/workspace/h-config/bitbucket-quo_access
 
@@ -46,6 +45,14 @@ elif [ "$1" = "$z_config" ]; then
   ssh-add ~/workspace/z-config/bitbucket_access
 
   echo Se ha modificado la configuración para JZetina
+elif [ "$1" = "$dt_config" ]; then
+  cp ~/workspace/h-config-dt/config ~/.ssh
+  cp ~/workspace/h-config/.gitconfig ~/
+
+  # TODO: Reemplzar por un bucle que recorra cada archivo ssh
+  ssh-add ~/workspace/h-config-dt/bitbucket-HazaelDesarrollaTech
+
+  echo Se ha modificado la configuración para HVite dt
 else
   echo La configuración introducida es invalida
 fi
